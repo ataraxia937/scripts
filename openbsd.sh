1ef7f31d03d1a8f8fb52324c270b7f46c4ea397a
Ray Kohler <ataraxia937@users.noreply.github.com>
2024-09-27 10:24:04 -0400

    openbsd: cleanup and simplify

--- openbsd.sh
+++ openbsd.sh
@@ -5,10 +5,9 @@ set -ex
 syspatch || true
 
 pkg_add ffmpeg firefox
-#pkg_add avahi cups
 
-rcctl enable unwind #messagebus avahi_daemon cupsd
-rcctl start unwind #messagebus avahi_daemon cupsd
+rcctl enable unwind
+rcctl start unwind
 
 sed -i -e '73s/1536M/infinity/' /etc/login.conf
 
@@ -23,7 +22,6 @@ hostname lunaria
 cat > /etc/hosts <<HOSTS
 127.0.0.1       localhost lunaria
 ::1             localhost lunaria
-192.168.0.35    printer.local printer
 HOSTS
 
 cat > /etc/daily.local <<DAILY
@@ -32,20 +30,12 @@ fw_update
 pkg_add -u
 DAILY
 
-#lpadmin -p lp -E -v ipp://printer.local:631/ipp/print -m everywhere
-#lpadmin -d lp
-
 cat >> /root/.profile <<ROOTPROFILE
 umask 022
 export HISTFILE="\$HOME/.ksh_history"
 PS1='\u:\w:\!:\\$ '
 alias ls='ls -F'
 ROOTPROFILE
-
-#cat > /home/ataraxia/.cwmrc <<CWM
-#gap 36 0 0 0
-#ignore xclock
-#CWM
 
 cat >> /home/ataraxia/.profile <<USERPROFILE
 umask 077
@@ -77,10 +67,6 @@ Xft.hinting:1
 Xft.antialias:1
 Xft.rgba:rgb
 XLock*dpmsoff:5
-XClock*analog:false
-XClock*face:mono
-XClock*render:true
-XClock*strftime:%a %b %e %l:%M
 XDEFAULTS
 
 mkdir -p /home/ataraxia/.config/gtk-3.0
@@ -95,9 +81,7 @@ export LC_CTYPE="en_US.UTF-8"
 setxkbmap -option compose:caps
 xset s off
 xset dpms 0 0 0
-#xclock -geometry -0+0 &
-xterm -geometry +0+0 &
-xterm -geometry +0-0 &
+xterm &
 firefox &
 exec cwm
 XSESSION
