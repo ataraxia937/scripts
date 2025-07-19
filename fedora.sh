2abb1aaefdb2454dfc2db93c560cbcaa66894141
ataraxia937 <ataraxia937@noreply.codeberg.org>
2025-07-19 13:32:47 -0400

    feat: add fedora.sh

---
+++ fedora.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo '%wheel ALL=(ALL) NOPASSWD:/usr/bin/dnf update' | sudo tee /etc/sudoers.d/dnf
+sudo chmod 440 /etc/sudoers.d/dnf
+
+echo 'kernel.yama.ptrace_scope = 3' | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
+
+sudo dnf -y update
+
+sudo dnf -y install cargo dotnet-sdk-8.0 fish gh golang google-chrome-stable node pylint restic rust-src rustfmt shellcheck typescript vim-default-editor yarnpkg
+
+sudo tee /etc/yum.repos.d/vscode.repo <<EOF
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+autorefresh=1
+type=rpm-md
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+
+sudo dnf -y install code
+
+chsh -s /bin/fish
