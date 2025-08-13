bba37ea40c4da00d6d272f1d5ee58173335b6f14
ataraxia937 <ataraxia937@noreply.codeberg.org>
2025-08-13 10:57:29 -0400

    feat: add debian setup script

---
+++ debian.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo '%sudo ALL=(ALL) NOPASSWD:/usr/bin/apt update' | sudo tee /etc/sudoers.d/apt
+echo '%sudo ALL=(ALL) NOPASSWD:/usr/bin/apt upgrade' | sudo tee -a /etc/sudoers.d/apt
+sudo chmod 440 /etc/sudoers.d/apt
+sudo visudo -c
+
+echo 'kernel.yama.ptrace_scope = 3' | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
+
+sudo apt -y update
+sudo apt -y upgrade
+
+sudo apt -y install apt-file build-essentials curl git restic vim xxd
+sudo apt -y --purge autoremove nano
+
+cd /tmp || exit
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt -y install ./google-chrome-stable_current_amd64.deb
+sudo /etc/cron.daily/google-chrome
+
+wget -O vscode.deb 'https://go.microsoft.com/fwlink/?LinkID=760868'
+echo "code code/add-microsoft-repo boolean true" | sudo debconf-set-selections
+sudo apt -y install ./vscode.deb
+
+restic restore latest -t /
