ef1fe220b824e400d71ac40efc6daeb4aa2f827b
ataraxia937 <ataraxia937@noreply.codeberg.org>
2025-08-23 11:05:09 -0400

    debian: fish

--- debian.sh
+++ debian.sh
@@ -11,10 +11,12 @@ sudo sysctl -p
 sudo apt -y update
 sudo apt -y upgrade
 
-sudo apt -y install apt-file build-essential curl fd-find flatpak fonts-noto fzf gawk git imagemagick libsqlite3-dev luarocks needrestart-session podman ptyxis python3-pip python3-pynvim python3-venv restic ruby ruby-dev sqlite3 vim xclip xxd
+sudo apt -y install apt-file build-essential curl fd-find fish flatpak fonts-noto fzf gawk git imagemagick libsqlite3-dev luarocks needrestart-session podman ptyxis python3-pip python3-pynvim python3-venv restic ruby ruby-dev sqlite3 vim xclip xxd
 sudo apt -y --purge autoremove fonts-dejavu\* gnome-terminal nano
 
 sudo flatpak remote-add flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+chsh -s /usr/bin/fish
 
 cd /tmp || exit
 
