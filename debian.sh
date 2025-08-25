5465d63a0297db1788737f1a6af150d9777c6101
ataraxia937 <ataraxia937@noreply.codeberg.org>
2025-08-25 14:26:39 -0400

    neovim -> vscode

--- debian.sh
+++ debian.sh
@@ -11,9 +11,18 @@ sudo sysctl -p
 sudo apt -y update
 sudo apt -y upgrade
 
-sudo apt -y install apt-file build-essential curl fd-find fish flatpak fonts-noto fzf gawk git imagemagick libsqlite3-dev luarocks needrestart-session podman ptyxis python3-pip python3-pynvim python3-venv restic ruby ruby-dev sqlite3 vim xclip xxd
+sudo apt -y install apt-file build-essential curl fish flatpak fonts-noto gawk git needrestart-session podman ptyxis python3-pip python3-venv restic ruby ruby-dev sqlite3 vim xclip xxd
 sudo apt -y --purge autoremove fonts-dejavu\* gnome-terminal nano
 
 sudo flatpak remote-add flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 chsh -s /usr/bin/fish
+
+cd /tmp || exit
+
+# VS Code
+wget -O vscode.deb 'https://go.microsoft.com/fwlink/?LinkID=760868'
+echo "code code/add-microsoft-repo boolean true" | sudo debconf-set-selections
+sudo apt -y install ./vscode.deb
+
+restic restore latest -t /
