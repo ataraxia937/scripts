ae73cf1b06517f95e8214c61ecb3e3cdb87e554b
ataraxia937 <ataraxia937@noreply.codeberg.org>
2025-09-22 11:50:08 -0400

    debian: revert to full setup

--- debian.sh
+++ debian.sh
@@ -8,10 +8,12 @@ sudo visudo -c
 echo 'kernel.yama.ptrace_scope = 3' | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
 
+sudo sed -i -e '/daemon/aAutomaticLoginEnable=True\nAutomaticLogin=ataraxia' /etc/gdm3/daemon.conf
+
 sudo apt -y update
 sudo apt -y upgrade
 
-sudo apt -y install apt-file chrony curl flatpak fonts-noto git needrestart-session podman ptyxis restic sqlite3 ufw vim xclip xxd
+sudo apt -y install apt-file build-essential chrony curl flatpak fonts-noto gawk git needrestart-session podman ptyxis python3-pip python3-venv restic sqlite3 ufw vim xclip xxd
 sudo apt -y --purge autoremove fonts-dejavu\* gnome-terminal nano
 
 sudo flatpak remote-add flathub https://dl.flathub.org/repo/flathub.flatpakrepo
@@ -19,4 +21,14 @@ sudo flatpak remote-add flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 sudo ufw enable
 
 cd /tmp || exit
+
+# Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt -y install ./google-chrome-stable_current_amd64.deb
+sudo /etc/cron.daily/google-chrome
+
+# VS Code
+wget -O vscode.deb 'https://go.microsoft.com/fwlink/?LinkID=760868'
+echo "code code/add-microsoft-repo boolean true" | sudo debconf-set-selections
+sudo apt -y install ./vscode.deb
 
