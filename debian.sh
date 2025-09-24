4fd450e016fe171e23acfea4c116b39911d6711c
ataraxia937 <ataraxia937@noreply.codeberg.org>
2025-09-24 18:07:05 -0400

    debian: add nix (deb), remove system python stuff

--- debian.sh
+++ debian.sh
@@ -5,18 +5,18 @@ echo '%sudo ALL=(ALL) NOPASSWD:/usr/bin/apt upgrade' | sudo tee -a /etc/sudoers.
 sudo chmod 440 /etc/sudoers.d/apt
 sudo visudo -c
 
-sudo usermod -aG adm ataraxia
-
 echo 'kernel.yama.ptrace_scope = 3' | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
 
 sudo apt -y update
 sudo apt -y upgrade
 
-sudo apt -y install apt-file build-essential chrony curl flatpak fonts-noto gawk git needrestart-session podman ptyxis python3-pip python3-venv restic sqlite3 ufw vim xclip xxd
+sudo apt -y install apt-file build-essential chrony curl flatpak fonts-noto gawk git needrestart-session nix-setup-systemd podman ptyxis restic sqlite3 ufw vim xclip xxd
 sudo apt -y --purge autoremove fonts-dejavu\* gnome-terminal nano
 
 sudo flatpak remote-add flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+sudo usermod -aG adm,nix-users ataraxia
 
 sudo ufw enable
 
