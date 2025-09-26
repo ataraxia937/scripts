335f63d50514e202a124db40ebc213555077f466
ataraxia937 <ataraxia937@noreply.codeberg.org>
2025-09-26 09:54:41 -0400

    configure quad9 with DoT

--- debian.sh
+++ debian.sh
@@ -1,25 +1,43 @@
 #!/bin/sh
 
+# Update software
+sudo apt -y update
+sudo apt -y upgrade
+
+# Configure DNS
+conn="$(nmcli --terse --field NAME connection show --active | head -n1)"
+sudo nmcli connection modify "$conn" ipv4.dns '9.9.9.9#dns.quad9.net,149.112.112.112#dns.quad9.net'
+sudo nmcli connection modify "$conn" ipv4.ignore-auto-dns yes
+sudo nmcli connection modify "$conn" ipv6.dns '2620:fe::fe#dns.quad9.net,2620:fe::9#dns.quad9.net'
+sudo nmcli connection modify "$conn" ipv6.ignore-auto-dns yes
+sudo nmcli connection modify "$conn" connection.dns-over-tls 2
+sudo nmcli connection down "$conn"
+sudo nmcli connection up "$conn"
+
+# Configure sudo
 echo '%sudo ALL=(ALL) NOPASSWD:/usr/bin/apt update' | sudo tee /etc/sudoers.d/apt
 echo '%sudo ALL=(ALL) NOPASSWD:/usr/bin/apt upgrade' | sudo tee -a /etc/sudoers.d/apt
 sudo chmod 440 /etc/sudoers.d/apt
 sudo visudo -c
 
+# Harden ptrace
 echo 'kernel.yama.ptrace_scope = 3' | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
 
-sudo apt -y update
-sudo apt -y upgrade
-
+# Install / remove debs
 sudo apt -y install apt-file build-essential chrony curl flatpak fonts-noto gawk git jq needrestart-session podman ptyxis sqlite3 ufw vim xclip xxd
 sudo apt -y --purge autoremove cups-browsed fonts-dejavu\* gnome-terminal nano
 
+# Configure Flatpak
 sudo flatpak remote-add flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
+# Configure group memberships
 sudo usermod -aG adm ataraxia
 
+# Enable firewall
 sudo ufw enable
 
+# Move to /tmp before downloading things
 cd /tmp || exit
 
 # Chrome
--- fedora.sh
+++ fedora.sh
@@ -1,16 +1,31 @@
 #!/bin/sh
 
+# Set hostname
 sudo hostnamectl hostname lunaria
 
+# Configure DNS
+conn="$(nmcli --terse --field NAME connection show --active | head -n1)"
+sudo nmcli connection modify "$conn" ipv4.dns '9.9.9.9#dns.quad9.net,149.112.112.112#dns.quad9.net'
+sudo nmcli connection modify "$conn" ipv4.ignore-auto-dns yes
+sudo nmcli connection modify "$conn" ipv6.dns '2620:fe::fe#dns.quad9.net,2620:fe::9#dns.quad9.net'
+sudo nmcli connection modify "$conn" ipv6.ignore-auto-dns yes
+sudo nmcli connection modify "$conn" connection.dns-over-tls 2
+sudo nmcli connection down "$conn"
+sudo nmcli connection up "$conn"
+
+# Configure sudo
 echo '%wheel ALL=(ALL) NOPASSWD:/usr/bin/dnf update' | sudo tee /etc/sudoers.d/dnf
 sudo chmod 440 /etc/sudoers.d/dnf
 sudo visudo -c
 
+# Configure auto-login
 sudo sed -i -e '/daemon/aAutomaticLoginEnable=True\nAutomaticLogin=ataraxia' /etc/gdm/custom.conf
 
+# Harden ptrace
 echo 'kernel.yama.ptrace_scope = 3' | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
 
+# Configure firewall
 sudo firewall-cmd --remove-service=ssh
 sudo firewall-cmd --remove-service=samba-client
 sudo firewall-cmd --remove-port=1025-65535/udp
@@ -19,17 +34,27 @@ sudo firewall-cmd --add-service=mdns
 sudo firewall-cmd --set-log-denied=all
 sudo firewall-cmd --runtime-to-permanent
 
+# Update software
 sudo dnf -y update
 
-sudo dnf -y install --allowerasing awscli2 gh mozilla-ublock-origin restic vim-default-editor
+# Install packages
+sudo dnf -y install --allowerasing google-chrome-stable vim-default-editor
 
-sudo dnf -y install "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
-sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
-
-sudo dnf -y swap --allowerasing ffmpeg-free ffmpeg
-sudo dnf -y install intel-media-driver
-
+# Clean up unneeded packages
 sudo dnf -y autoremove
 
+# Configure Flatpak
 flatpak remote-add flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
+# Install VS Code
+sudo tee /etc/yum.repos.d/vscode.repo << EOF
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+autorefresh=1
+type=rpm-md
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+sudo dnf -y install code
