b82a78021910435d9f36245560b07ffd8337b2ad
ataraxia937 <ataraxia937@noreply.codeberg.org>
2025-10-03 20:43:19 -0400

    chromeos: remove vscode and switch to tiny vim

--- chromeos.sh
+++ chromeos.sh
@@ -3,7 +3,7 @@
 sudo apt -y update
 sudo apt -y upgrade
 
-sudo apt -y install apt-file build-essential curl flatpak gawk git podman sqlite3 vim xxd
+sudo apt -y install apt-file build-essential curl flatpak gawk git podman sqlite3 vim-tiny
 sudo apt -y --purge autoremove cups-browsed nano
 
 sudo flatpak remote-add flathub https://dl.flathub.org/repo/flathub.flatpakrepo
@@ -12,11 +12,3 @@ sudo usermod --add-subuids 100000-165535 --add-subgids 100000-165535 ataraxia
 podman system migrate
 
 sudo usermod -aG adm ataraxia
-
-cd /tmp || exit
-
-# VS Code
-wget -O vscode.deb 'https://go.microsoft.com/fwlink/?LinkID=760868'
-echo "code code/add-microsoft-repo boolean true" | sudo debconf-set-selections
-sudo apt -y install ./vscode.deb
-
