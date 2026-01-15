9d1b8369d60e706c65fc381f753315ab480d6da2
ataraxia937 <ataraxia937@noreply.codeberg.org>
2026-01-15 17:39:10 +0100

    Update chromeos.sh

--- chromeos.sh
+++ chromeos.sh
@@ -14,10 +14,9 @@ curl https://mise.run | sh
 echo "eval \"\$(/home/ataraxia/.local/bin/mise activate bash)\"" >> ~/.bashrc
 eval "$(/home/ataraxia/.local/bin/mise activate bash)"
 
-curl -Lo /tmp/font.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/NerdFontsSymbolsOnly.zip
-mkdir $HOME/.local/share/fonts
-unzip -d $HOME/.local/share/fonts/ /tmp/font.zip SymbolsNerdFontMono-Regular.ttf
-fc-cache
+mkdir $HOME/.config/mise
+curl -Lo $HOME/.config/mise/config.toml https://codeberg.org/ataraxia937/scripts/raw/branch/main/configs/mise_config.toml
+mise -C $HOME install
 
 mkdir $HOME/.config/alacritty
 curl -Lo $HOME/.config/alacritty/alacritty.toml https://codeberg.org/ataraxia937/scripts/raw/branch/main/configs/alacritty.toml
