8a6f9414084bc235ee32781c9da22d6099dcd5c3
ataraxia937 <ataraxia937@noreply.codeberg.org>
2026-01-15 16:33:06 +0100

    ChromeOS: install symbols Nerd Font

--- chromeos.sh
+++ chromeos.sh
@@ -13,3 +13,8 @@ sudo flatpak remote-add flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 curl https://mise.run | sh
 echo "eval \"\$(/home/ataraxia/.local/bin/mise activate bash)\"" >> ~/.bashrc
 eval "$(/home/ataraxia/.local/bin/mise activate bash)"
+
+curl -Lo /tmp/font.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/NerdFontsSymbolsOnly.zip
+mkdir $HOME/.local/share/fonts
+unzip -d $HOME/.local/share/fonts/ /tmp/font.zip SymbolsNerdFontMono-Regular.ttf
+fc-cache
