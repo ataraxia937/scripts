0f60d72e6ff860e47a2a469efac32d5bc9dabdaf
ataraxia937 <ataraxia937@noreply.codeberg.org>
2026-01-24 04:20:11 +0100

    Update chromeos.sh

--- chromeos.sh
+++ chromeos.sh
@@ -6,7 +6,7 @@ sudo apt -y update
 sudo apt -y full-upgrade
 sudo apt -y --purge autoremove
 
-sudo apt -y install apt-file build-essential flatpak jq rsync
+sudo apt -y install apt-file build-essential dc flatpak jq ncdu rsync
 
 sudo flatpak remote-add flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
