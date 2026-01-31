65ccf44e7adec3d24c0fa7726afe61a143127cbc
ataraxia937 <ataraxia937@noreply.codeberg.org>
2026-01-31 17:35:29 +0100

    Update chromeos.sh

--- chromeos.sh
+++ chromeos.sh
@@ -8,25 +8,27 @@ sudo apt -y --purge autoremove
 
 sed -i -e '/^#shopt -s globstar$/s/^#//' \
   -e '/^#force_color_prompt=yes$/s/^#//' \
-  -e '/^#\[ -x \/usr\/bin\/lesspipe \] && eval "\$(SHELL=\/bin\/sh lesspipe)"$/s/^#//' $HOME/.bashrc
+  -e '/^#\[ -x \/usr\/bin\/lesspipe \] && eval "\$(SHELL=\/bin\/sh lesspipe)"$/s/^#//' ~/.bashrc
 
 sudo apt -y install apt-file build-essential dc fd-find flatpak git jq ncdu ripgrep rsync unzip
 
 sudo flatpak remote-add flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
+echo "export npm_config_ignore_scripts=true" >> ~/.bashrc
+
 curl https://mise.run | sh
 echo "eval \"\$(/home/ataraxia/.local/bin/mise activate bash)\"" >> ~/.bashrc
 eval "$(/home/ataraxia/.local/bin/mise activate bash)"
 
-mkdir $HOME/.config/mise
-curl -Lo $HOME/.config/mise/config.toml https://codeberg.org/ataraxia937/scripts/raw/branch/main/configs/mise_config.toml
-mise -C $HOME install
+mkdir ~/.config/mise
+curl -Lo ~/.config/mise/config.toml https://codeberg.org/ataraxia937/scripts/raw/branch/main/configs/mise_config.toml
+mise install
 
 curl -Lo /tmp/font.zip
-mkdir -p $HOME/.local/share/fonts
-unzip -d $HOME/.local/share/fonts /tmp/font.zip
+mkdir -p ~/.local/share/fonts
+unzip -d ~/.local/share/fonts /tmp/font.zip
 fc-cache
 
 sudo gpasswd -a ataraxia937 render
-mkdir -p $HOME/.config/kitty
-curl -Lo $HOME/.config/kitty/kitty.conf https://codeberg.org/ataraxia937/scripts/raw/branch/main/configs/kitty.conf
+mkdir -p ~/.config/kitty
+curl -Lo ~/.config/kitty/kitty.conf https://codeberg.org/ataraxia937/scripts/raw/branch/main/configs/kitty.conf
